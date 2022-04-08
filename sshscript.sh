7db7e8f1cf2b9e8eb9a271d3ca53aba11efc8b1b
NamBui98 <bvnam98@gmail.com>
2022-04-08 10:14:23 +0700

    update

--- sshscript.sh
+++ sshscript.sh
@@ -3,21 +3,8 @@ spawn ssh root@139.59.229.139
 send "yes\r"
 expect "Password:*"
 send "bvNam98hy\r"
+expect "$ "
 
-expect {
-    -re ".*es.*o.*" {
-        exp_send "yes\r"
-        exp_continue
-    }
-    -re ".*sword.*" {
-        exp_send "$pass\r"
-    }
-}
-exp_send "ls\r"
-exp_send "exit\r"
-expect {
-    eof {puts "Connection closed"}
-    timeout {puts "Connection timed out"}
-}
 interact
+
 
