0eb496287c84fd0419c0896190d9a5e3a12c6438
NamBui98 <bvnam98@gmail.com>
2022-04-08 10:11:25 +0700

    update

--- sshscript.sh
+++ sshscript.sh
@@ -1,11 +1,19 @@
 #!/usr/bin/expect -f
-spawn ssh root@139.59.229.139 << EOF
-    ls
-EOF
+spawn ssh root@139.59.229.139 
 send "yes\r"
 expect "Password:*"
 send "bvNam98hy\r"
 expect "$ "
-
+expect {
+    -re ".*es.*o.*" {
+        exp_send "yes\r"
+        exp_continue
+    }
+    -re ".*sword.*" {
+        exp_send "$pass\r"
+    }
+}
+exp_send "ls\r"
+interact
 # interact
 
