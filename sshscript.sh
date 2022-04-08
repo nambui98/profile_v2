25c0a568b5d418cb3ef6be6574867952fe985e64
NamBui98 <bvnam98@gmail.com>
2022-04-08 10:06:37 +0700

    update

--- sshscript.sh
+++ sshscript.sh
@@ -1,9 +1,15 @@
 #!/usr/bin/expect -f
-spawn ssh root@139.59.229.139 'ls'
+spawn ssh root@139.59.229.139 '#!/usr/bin/env bash
+
+cd `dirname $BASH_SOURCE`
+
+ls
+
+cd - > /dev/null'
 send "yes\r"
 expect "Password:*"
 send "bvNam98hy\r"
 expect "$ "
 
-interact
+# interact
 
