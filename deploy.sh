3901d22bd90091637e180226b1df08f89d2d0cd3
NamBui98 <bvnam98@gmail.com>
2022-04-08 09:30:13 +0700

    update

--- deploy.sh
+++ deploy.sh
@@ -1,7 +1,11 @@
 #!/usr/bin/env bash
 ls
-cd `profile_v2`
-git pull
-npm i
-npm run build
-cp -r build/* /var/www/html
+cd ..
+ls 
+cd ..
+ls
+# cd `profile_v2`
+# git pull
+# npm i
+# npm run build
+# cp -r build/* /var/www/html
