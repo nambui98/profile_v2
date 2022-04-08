f821b64ebc3ead0e3578eac5e8fc2ceebe145d29
NamBui98 <bvnam98@gmail.com>
2022-04-08 09:26:45 +0700

    update

--- deploy.sh
+++ deploy.sh
@@ -1,8 +1,7 @@
-#!/bin/sh
-ssh root@139.59.229.139<<EOF
-   cd /var/www/html/
-   git pull origin master
-   npm install --production
-   pm2 restart all
-   exit
-EOF
+#!/usr/bin/env bash
+
+cd `profile_v2`
+git pull
+npm i
+npm run build
+cp -r build/* /var/www/html
